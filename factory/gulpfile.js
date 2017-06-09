8447d8fafbebc25a338b7a44e88d7ab660727400
Lucia <lucia@bahai.us>
2017-06-09 14:26:52 -0500

    installing gulp and gulp-sass

--- factory/gulpfile.js
+++ factory/gulpfile.js
@@ -1,25 +1,26 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var sass = require('gulp-sass');
+var $ = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+    'bower_components/normalize.scss/sass',
+    'bower_components/foundation-sites/scss',
+    'bower_components/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+    return gulp.src('static/stylesheets/sass/main.sass')
+        .pipe($.sass({
+                includePaths: sassPaths,
+                outputStyle: 'compressed' // if css compressed **file size**
+            })
+            .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe(gulp.dest('static/stylesheets/css'));
 });
 
 gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+    gulp.watch(['static/stylesheets/sass'], ['sass']);
 });
