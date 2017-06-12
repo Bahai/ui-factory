4e61ed64fb1cd687aebb14da3aee55cbdfe1908e
Lucia <lucia@bahai.us>
2017-06-12 11:13:59 -0500

    fixing views

--- factory/urls.py
+++ factory/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^tabhome', views.TAB_index, name='TAB'),
+    url(r'^tabarticle', views.TAB_article, name='TAB_article'),
+    url(r'^login', views.login, name='login'),
 ]
--- factory/views.py
+++ factory/views.py
@@ -6,3 +6,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'factory/home.html', {})
+
+def TAB_index(request):
+    return render(request, 'TAB/blog_index_page.html', {})
+
+def TAB_article(request):
+    return render(request, 'TAB/blog_page.html', {})
+
+def login(request):
+    return render(request, 'login/index.html', {})
