75667f1fc119614b69b52898f12081933d0f8400
Lucia <lucia@bahai.us>
2017-06-16 16:48:03 -0500

    adding static file dir

--- ui/settings.py
+++ ui/settings.py
@@ -148,3 +148,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, './factory/static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
