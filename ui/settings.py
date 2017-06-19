04976177acf2aac5b6f178bf72ecb99b57ad6582
Lucia <lucia@bahai.us>
2017-06-19 10:06:23 -0500

    trying another static root

--- ui/settings.py
+++ ui/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'factory/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/factory/static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
