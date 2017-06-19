c9a77561ca5caf89b9d6f12c61ae83683e0248e0
Lucia <lucia@bahai.us>
2017-06-19 10:48:08 -0500

    removing admin from urls

--- ui/urls.py
+++ ui/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'', include('factory.urls')),
 ]
 
