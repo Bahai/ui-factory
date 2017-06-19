bb1d99c562f18e000f85b9ec5fc0cc9e296286a1
Lucia <lucia@bahai.us>
2017-06-19 11:03:18 -0500

    clean up

--- ui/settings.py
+++ ui/settings.py
@@ -148,8 +148,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, './factory/static')
-
-# Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
