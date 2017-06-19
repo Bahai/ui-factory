b95e327be1095bbada2988b221cf9c96cb189bb5
Lucia <lucia@bahai.us>
2017-06-19 10:39:58 -0500

    adding static root to urls

--- ui/urls.py
+++ ui/urls.py
@@ -23,3 +23,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('factory.urls')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
